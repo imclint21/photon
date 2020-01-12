65ee9096ecb906d1737402f33f63f851b9e146d1
Clint.Network <me@clint.network>
2020-01-12 02:12:32 +0100

    Remove active item

--- example/views/_layout.php
+++ example/views/_layout.php
@@ -15,7 +15,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarColor01">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
+                    <li class="nav-item">
                         <a class="nav-link" href="/example/home/index">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
