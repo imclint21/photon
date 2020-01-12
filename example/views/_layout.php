48d7cc72f3a70936104cfdbab4d83fecbe0c6c7b
Clint.Network <me@clint.network>
2020-01-12 02:44:25 +0100

    Change index link

--- example/views/_layout.php
+++ example/views/_layout.php
@@ -8,7 +8,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">💡 Photon PHP Framework</a>
+            <a class="navbar-brand" href="/example">💡 Photon PHP Framework</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
                 aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
